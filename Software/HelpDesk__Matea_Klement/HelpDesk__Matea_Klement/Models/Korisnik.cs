2c88eaaec54f3387175910287dc8dbf046cdc0d3
Matea Klement <mklement21@foi.hr>
2023-05-26 23:40:45 +0200

    Kreiranje tablica

--- Software/HelpDesk__Matea_Klement/HelpDesk__Matea_Klement/Models/Korisnik.cs
+++ Software/HelpDesk__Matea_Klement/HelpDesk__Matea_Klement/Models/Korisnik.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelpDesk__Matea_Klement.Models {
     public abstract class Korisnik {
         public int IdKorisnik { get; set; }
-        public string Email { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
         public string BrojMobitela { get; set; }
         public string KorisnickoIme { get; set; }
         public string Lozinka { get; set; }
