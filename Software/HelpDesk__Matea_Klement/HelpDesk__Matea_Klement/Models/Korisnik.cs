1db7b2cf6ce41250a98845d3f62f661f7b0fdb79
Matea Klement <mklement21@foi.hr>
2023-05-26 23:07:10 +0200

    Models kreiranje

---
+++ Software/HelpDesk__Matea_Klement/HelpDesk__Matea_Klement/Models/Korisnik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk__Matea_Klement.Models {
+    public abstract class Korisnik {
+        public int IdKorisnik { get; set; }
+        public string Email { get; set; }
+        public string BrojMobitela { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public string LozinkaNova { get; set; }
+    }
+}
---
+++ Software/HelpDesk__Matea_Klement/HelpDesk__Matea_Klement/Models/Zahtjev.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk__Matea_Klement.Models {
+    public abstract class Zahtjev {
+        public int IdZahtjev { get; set; }
+        public string ZahtjevNaslov { get; set; }
+        public string ZahtjevOpis { get; set; }
+        public DateTime ZahtjevVrijeme { get; set; }
+    }
+}
